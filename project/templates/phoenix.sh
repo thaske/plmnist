6bd1d2e9236451a3b41f7166ced1e86daa93a6b7
Brad Crawford <bcrawford@gatech.edu>
2024-04-02 17:27:54 -0400

    added cpu_per_task and signac init statement

--- project/templates/phoenix.sh
+++ project/templates/phoenix.sh
@@ -1,8 +1,8 @@
 {% extends "slurm.sh" %}
 
 {% block header %}
-{% set gpus = operations|map(attribute='directives.ngpu')|max %}
-{% set mem_per_cpu = operations|map(attribute='directives.mem-per-cpu')|max  %}
+{% set np = operations|map(attribute='directives.np')|sum %}
+{% set mem_per_cpu = operations|map(attribute='directives.mem-per-cpu')|max %}
 {% set cpus_per_task = operations|map(attribute='directives.cpus-per-task')|max  %}
 {% set gpus_per_task = operations|map(attribute='directives.gpus-per-task')|max  %}
 
@@ -10,6 +10,7 @@
 
 {% if gpus_per_task %}
 #SBATCH -p gpu-a100
+#SBATCH --gres gpu:{{ np_global * gpus_per_task }}
 #SBATCH --gpus-per-task={{ gpus_per_task }}
 
 {%- else %}
@@ -24,6 +25,7 @@
 #SBATCH --output=/dev/null
 #SBATCH --error=/dev/null
 #SBATCH --mem-per-cpu={{ mem_per_cpu }}G
+
 
 echo  "Running on host" hostname
 echo  "Time is" date
